04f9a42e6b69568d30d2e550fb41ed79c764d425
Drew Cowan <dcowan@labattfood.com>
2014-05-16 12:57:32 -0500

    Things that are no longer ignored...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstRailsApp::Application.config.secret_key_base = 'b6c9b8322e93abc1083a26559998d0511b7a8cc0889605958bcbd4521d0e816771c8064e8164e4f612c4c429a62c3ce1fdf711dc2e079ed09d441054569a953a'
